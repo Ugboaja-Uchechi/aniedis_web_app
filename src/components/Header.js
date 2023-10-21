70548e93e5cd33de4c169d8925d3bd00a34ad5ad
ugboaja-uchechi <stephanieuchechi754@gmail.com>
2023-10-22 00:23:29 +0100

    Delete unnecessary files, create and style header component

---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { ReactComponent as ArrowDown } from "../images/arrow_down.svg";
+
+const Header = () => {
+  return (
+    <header className="bg-[#1a1a1a] flex justify-between items-center py-3.5 px-16">
+      <div>
+        <h4 className="text-xl font-semibold text-white uppercase">aniedi udo-obong</h4>
+      </div>
+      <div className="flex items-center">
+        <div className="flex pe-20">
+          <p className="text-lg font-normal text-white uppercase opacity-50 pe-10">Who is Aniedi?</p>
+          <div className="flex items-center">
+            <p className="text-lg font-normal text-white uppercase opacity-50 pe-2">media</p>
+            <ArrowDown className="opacity-50" />
+          </div> 
+        </div>
+
+        <button className="uppercase text-white bg-[#030304] bg-opacity-50 border-white border-[2px] border-opacity-30 px-6 py-2 rounded-3xl tracking-wide">Contact</button>
+      </div>
+
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/page/Home.js
@@ -0,0 +1,17 @@
+import Header from "../components/Header";
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <main className="bg-[#1a1a1a] h-screen">
+        {/* grid then a flex for the 2nd div */}
+        <div></div>
+        <div></div>
+      </main>
+    </>
+
+  );
+};
+
+export default Home
