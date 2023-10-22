93f7c0e7864ec647afc36fe2f654446c03617d80
ugboaja-uchechi <stephanieuchechi754@gmail.com>
2023-10-22 21:25:25 +0100

    Style testimonial section

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,7 +1,7 @@
 import Header from "../components/Header";
 import { ReactComponent as Arrow } from "../images/arrow.svg"
-
 import aniedi from "../images/aniedi.png"
+import Testimonial from "../components/Testimonial";
 
 const Home = () => {
   return (
@@ -20,6 +20,7 @@ const Home = () => {
           <img src={aniedi} alt="A face art of Aniedi Udo-Obong" />
         </div>
       </main>
+      <Testimonial />
     </>
 
   );
